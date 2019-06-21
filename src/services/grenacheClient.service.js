40d5f120e6633765c03fd9cbcae0992b1756aa24
ZIMkaRU <vsvoronkov@gmail.com>
2019-06-21 15:48:45 +0300

    move checking of grenache client configs to separate module

--- src/services/grenacheClient.service.js
+++ src/services/grenacheClient.service.js
@@ -4,28 +4,13 @@ const Grenache = require('grenache-nodejs-http')
 const Link = require('grenache-nodejs-link')
 const config = require('config')
 const { CustomLogger } = require('./log.service')
+const { checkGrenacheClientConf } = require('./helpers')
+
+checkGrenacheClientConf()
 
 const logger = new CustomLogger({
-  label: ':grenache:client'
+  label: ':grenache:client:http'
 }).createLogger()
-
-const _checkConf = () => {
-  if (
-    config.has('grenacheClient') &&
-    config.has('grenacheClient.grape') &&
-    config.has('grenacheClient.query')
-  ) {
-    return
-  }
-
-  const err = new Error('ERR_CONFIG_ARGS_NO_GRENACHE_CLIENT')
-
-  logger.error('Found %s at %s', 'error', err)
-
-  throw err
-}
-
-_checkConf()
 
 const gClientConf = config.get('grenacheClient')
 
