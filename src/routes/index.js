c4b8319089c13b8608968b420d7c63e254c0fb5e
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-05-06 12:43:13 +0300

    Re-add get-data endpoint as deprecated to compatible

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,3 +11,9 @@ const baseController = asyncErrorCatcher(controllers.baseController)
 
 router.post('/check-stored-locally', baseController.checkStoredLocally)
 router.post('/json-rpc', baseController.jsonRpc)
+
+/**
+ * @deprecated
+ * The endpoint will be removed in the future, now need to use `/json-rpc` one
+ */
+router.post('/get-data', baseController.jsonRpc)
