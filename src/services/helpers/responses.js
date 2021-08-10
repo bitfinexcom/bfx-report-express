9e80598e7453247bec40e5eabea425deb04e79cb
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-08-10 20:09:55 +0300

    Implement json rpc responder

--- src/services/helpers/responses.js
+++ src/services/helpers/responses.js
@@ -1,26 +1,66 @@
 'use strict'
 
-const success = (statusCode, responseModel, res) => {
-  res.status(statusCode)
-  res.json(responseModel)
+const { logger } = require('../log.service')
 
-  return res
-}
+const JSON_RPC_VERSION = '2.0'
 
-const failure = (statusCode, errorMessage, res, id = null) => {
-  res.status(statusCode)
-  res.json({
-    error: {
-      code: statusCode,
-      message: errorMessage
-    },
-    id
-  })
+const jsonRpcResponder = (req, res, rpcRes) => {
+  const _body = (
+    req.body &&
+    typeof req.body === 'object'
+  )
+    ? req.body
+    : {}
+  const { id: reqId = null } = _body
+
+  if (
+    !rpcRes ||
+    typeof rpcRes !== 'object' ||
+    typeof rpcRes.jsonrpc !== 'string' ||
+    rpcRes instanceof Error
+  ) {
+    const code = 500
+    const message = 'Internal Server Error'
+
+    logger.error(rpcRes.stack || rpcRes)
+
+    res.status(code)
+    res.json({
+      jsonrpc: JSON_RPC_VERSION,
+      error: { code, message, data: null },
+      id: reqId
+    })
+
+    return res
+  }
+
+  const { jsonrpc, error, id = reqId } = rpcRes
+
+  if (
+    error &&
+    typeof error === 'object'
+  ) {
+    const {
+      code = 500,
+      message = 'Internal Server Error',
+      data = null
+    } = error
+
+    res.status(code)
+    res.json({
+      jsonrpc,
+      error: { code, message, data },
+      id
+    })
+  }
+
+  res.status(200)
+  res.json(rpcRes)
 
   return res
 }
 
 module.exports = {
-  success,
-  failure
+  jsonRpcResponder,
+  JSON_RPC_VERSION
 }
