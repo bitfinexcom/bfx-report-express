87e7bbd6f5341796b803a554a3dfc826bb4113f2
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-08-10 20:10:58 +0300

    Add json responder to error middleware

--- src/middlewares/errors.middleware.js
+++ src/middlewares/errors.middleware.js
@@ -1,23 +1,7 @@
 'use strict'
 
-const {
-  helpers,
-  prepareErrorDataService
-} = require('../services')
-const { failure } = helpers.responses
+const { jsonRpcResponder } = require('../services/helpers')
 
 module.exports = (err, req, res, next) => {
-  const id = (req.body && req.body.id) || null
-
-  const {
-    code,
-    message
-  } = prepareErrorDataService(err)
-
-  failure(
-    code,
-    message,
-    res,
-    id
-  )
+  jsonRpcResponder(req, res, err)
 }
